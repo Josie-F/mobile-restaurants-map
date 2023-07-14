7e750a850b2c144867b5d679d9f889c342b3f490
Josie-F <josephine.f@outlook.dk>
2023-07-14 23:31:10 +0100

    cleanup in db file

--- database-service.js
+++ database-service.js
@@ -4,8 +4,6 @@ let db
 
 export const initiateDatabase = async () => {
     if (!db) {
-        console.log("initiateDatabase called")
-
         try {
             db = SQLite.openDatabase("restaurantPicker.db");
             db.transaction(transaction => {
@@ -16,28 +14,32 @@ export const initiateDatabase = async () => {
                 )
             },
                 (e) => {
-                    console.log(e, 'error')
+                    console.log('Error when creating the users table: ', e)
                 },
                 () => {
-                    console.log('success')
+                    console.log('Successfully created the users table')
                 })
         } catch (e) {
-            console.log("failure", e)
+            console.log("Failure when trying to intitiate the database: ", e)
         }
     }
 }
 
 export const addUser = (username, pass) => {
-    console.log('add user called')
     db.transaction(transaction => {
-        transaction.executeSql('INSERT INTO users (username, password) values (?, ?)', ['admin', 'admin'],
+        transaction.executeSql('INSERT INTO users (username, password) values (?, ?)', [username, password],
             (transactionObj, resultSet) => console.log('result: ', resultSet),
             (transactionObj, error) => console.log('Error when inserting into table: ', error))
-    })
+    },
+        (e) => {
+            console.log('error when adding the user: ', e)
+        },
+        () => {
+            console.log('Successfully added the user')
+        })
 }
 
 export const getUsers = () => {
-    console.log('get users called')
     db.transaction(transaction => {
         transaction.executeSql('SELECT * FROM users',
             null,
@@ -45,24 +47,29 @@ export const getUsers = () => {
                 console.log('users', { data: _array }),
             (transactionObj, error) => console.log('Error when retrieving users: ', error)
         )
-    })
+    },
+        (e) => {
+            console.log('error when retrieving users: ', e)
+        },
+        () => {
+            console.log('Successfully retrieved all users')
+        })
 }
 
 export const verifyUserCredentials = (username, password, callback) => {
-    console.log('calling verifyUserCredentials')
     db.transaction(transaction => {
-        console.log('josie')
         transaction.executeSql(
             `SELECT * FROM users WHERE username="${username}" AND password="${password}"`,
             null,
             (transactionObj, { rows: { _array } }) =>
                 callback(_array[0]),
-            (transactionObj, error) => console.log('Error when verifying ', error)
+            (transactionObj, error) => console.log('Error trying to select user from users table: ', error)
         )
-    }, (e) => {
-        console.log(e, 'error')
     },
+        (e) => {
+            console.log('error when verifying user credentials: ', e)
+        },
         () => {
-            console.log('working')
+            console.log("Successfully verified user's details")
         })
 }
